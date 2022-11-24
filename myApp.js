edabce626df7de4b20d886f33de93045dd772d74
Bruno Duarte <bruno.duarte411@gmail.com>
2022-11-24 13:12:31 -0300

    Add console.com to route /name

--- myApp.js
+++ myApp.js
@@ -35,6 +35,7 @@ app.get("/:word/echo", function (req, res) {
   res.json({ echo: req.params.word });
 });
 app.route("/name").get(function (req, res) {
+  console.log(req.query);
   res.json({ name: `${req.query.first} ${req.query.last}` });
 });
 
